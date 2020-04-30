6622f59046cdcc8dffdec5c585d03374a27cc04e
baymax88 <smartbay1@outlook.com>
2020-04-30 10:29:38 +0800

    build project

---
+++ client/build/precache-manifest.b41829749f33372c3cb1f75bd3ba830f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7353b3840763a9526a5e8911cac6f51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e84619fb9973611151ff",
+    "url": "/static/css/main.cd978270.chunk.css"
+  },
+  {
+    "revision": "ceaef5fc231ad58944b8",
+    "url": "/static/js/2.430b09fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.430b09fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84619fb9973611151ff",
+    "url": "/static/js/main.bd36d085.chunk.js"
+  },
+  {
+    "revision": "6cf02a51c6240ec4d2a7",
+    "url": "/static/js/runtime-main.42c17448.js"
+  }
+]);
